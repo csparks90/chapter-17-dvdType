540cda08facf2329b0073e6f85d79727e65df93f
cspar <cspar@DESKTOP-JM1VHQ2>
2022-05-06 18:48:51 -0500

    Add project files.

---
+++ chapter 17 dvdType/dvdListType.h
@@ -0,0 +1,35 @@
+//****************************************************************************
+//*Author: Cody Sparks														 *
+//*																			 *
+//*class dvdListType														 *
+//*This class specifies the members to implement a a list of DVDs.			 *
+//****************************************************************************
+
+#include <string>
+#include "unorderedLinkedList.h"
+#include "dvdType.h"
+
+using namespace std;
+
+class dvdListType :public unorderedLinkedList<dvdType>
+{
+public:
+	bool dvdSearch(string title) const;
+
+	bool isDVDAvailable(string title) const;
+
+	void dvdCheckOut(string title);
+
+	void dvdCheckIn(string title);
+
+	bool dvdCheckTitle(string title) const;
+
+	void dvdUpdateInStock(string title, int num);
+
+	void dvdSetCopiesInStock(string title, int num);
+
+	void dvdPrintTitle() const;
+
+private:
+	void searchDVDList(string title, bool& found, nodeType<dvdType>* &current) const;
+};
